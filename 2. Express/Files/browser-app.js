fbbe9b2e448e5d8923893f746146facf70c782c9
TamangSujan <moktansujanj5@gmail.com>
2022-01-06 17:30:26 +0545

    Files

---
+++ 2. Express/Files/browser-app.js
@@ -0,0 +1,12 @@
+const navToggle = document.querySelector('.nav-toggle')
+const links = document.querySelector('.links')
+
+navToggle.addEventListener('click', function () {
+  links.classList.toggle('show-links')
+})
+const navToggle = document.querySelector('.nav-toggle')
+const links = document.querySelector('.links')
+
+navToggle.addEventListener('click', function () {
+  links.classList.toggle('show-links')
+})
